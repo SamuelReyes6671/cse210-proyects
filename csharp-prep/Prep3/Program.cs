1ba297037b1534b38a63dbdc53e8e58f248ca777
SamuelReyes6671 <samuel.reyes.condori@email.com>
2024-04-26 12:29:58 -0500

    Prep3 fixed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Console.WriteLine("What is the magic number? ");
+        int numberMagic=int.Parse(Console.ReadLine());  
+
+        Random randomGenerator=new Random();
+        int number=randomGenerator.Next(1,101);
+
+        int guess=-1;
+
+        while(guess!=numberMagic)
+        {   
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());   
+       
+            if (numberMagic > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (numberMagic < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }   
+                   
+        }     
+    }  
 }
