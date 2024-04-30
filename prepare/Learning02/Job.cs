b80cc981fe73f8acf0aa429ce37498a82e69e121
SamuelReyes6671 <samuel.reyes.condori@email.com>
2024-04-30 11:51:56 -0500

    Learning2 FIxed

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,12 @@
+using System;
+public class Job{
+public string _company;
+public string _jobTittle;
+public int _starYear;
+public int _endYear;
+
+public void Display()
+{
+    Console.WriteLine($"{_jobTittle}({_company}) {_starYear}--{_endYear}");
+}
+}
