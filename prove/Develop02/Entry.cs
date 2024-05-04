b3bd524ec1a8ca5c613731bdb7dedbd8862583a7
SamuelReyes6671 <samuel.reyes.condori@email.com>
2024-05-03 19:52:38 -0500

    Terminando el proyecto de mensajes.

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Entry(string date)
+{
+
+    public string _date;
+    public string _prompText;
+    public string _entryText;
+
+    public Entry(string date, string prompText, string entryText) : this(date)
+    {
+        _date = date;
+        _prompText = prompText;
+        _entryText = entryText;
+    }
+
+    public void Display(){
+
+        
+    }
+}
---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+public class Journal(){
+    List<Entry>_entries = new List<Entry>();
+
+    public void AddEntry(Entry entry){
+        _entries.Add(entry);
+    }
+    public void DisplayAll(){
+       foreach(Entry entry in _entries){
+        Console.WriteLine($"{entry._date}:{entry._entryText}");
+       }
+    }
+    public void SaveToFile(string file) {
+        using (StreamWriter writer = new StreamWriter(file))
+        {
+            foreach (Entry entry in _entries)
+            {
+                writer.WriteLine($"{entry._date},{entry._prompText},{entry._entryText}");
+            }
+        }
+    }
+    public void LoadFromFile(string file){
+        _entries.Clear();
+        using (StreamReader reader = new StreamReader(file))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] parts = line.Split(',');
+                if (parts.Length == 3)
+                {
+                    string date = parts[0];
+                    string prompText = parts[1];
+                    string entryText = parts[2];
+                    _entries.Add(new Entry( date, prompText, entryText));
+                }
+            }
+        }
+    }
+}
