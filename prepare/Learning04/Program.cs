c6c3082aa38fb913f1e65a837da0856b2f7f39f3
SamuelReyes6671 <samuel.reyes.condori@email.com>
2024-05-14 00:36:52 -0500

    Codigo resuelto

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,17 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment tarea1=new Assignment("Samuel Reyes","Multiplication");
+        Console.WriteLine(tarea1.GetSummary());
+
+        MathAssignment tarea2=new MathAssignment("Samuel Reyes","fractions","7.3","8-19");
+        Console.WriteLine(tarea2.GetSummary());
+
+        WritingAssignment tarea3 = new WritingAssignment("Mary Waters","European","The Causes of World Was II");
+        Console.WriteLine(tarea3.GetSummary());
+        Console.WriteLine(tarea3.GetWritingInformation());
+    
+        
     }
 }
