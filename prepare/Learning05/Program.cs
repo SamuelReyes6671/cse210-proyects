c995d49ae37a0934f1f6b3bba07190950ba486be
SamuelReyes6671 <samuel.reyes.condori@email.com>
2024-05-22 11:35:17 -0500

    proyecto numero 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes= new List<Shape>();
+
+        Square s1= new Square("red",5);
+        shapes.Add(s1);
+
+        Rectangle r1= new Rectangle("amarillo",3,4);
+        shapes.Add(r1);
+
+        Circle c1= new Circle("verde",3);
+        shapes.Add(c1);
+
+        foreach(Shape s in shapes){
+            string color=s.GetColor();
+            double area=s.GetArea();    
+
+            Console.WriteLine("The color "+color +" has an area of "+area);
+        }
+
     }
 }
